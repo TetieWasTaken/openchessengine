c84b438767bccb4a1ca2bf0af26b7d02e5540fed
TetieWasTaken <tjvssr@gmail.com>
2025-02-16 20:17:35 +0100

    feat(test): add bishop

---
+++ test/core/MoveGenerator/bishop.test.ts
@@ -0,0 +1,50 @@
+import { Board } from "../../../src/core/Board";
+import { MoveGenerator } from "../../../src/core/MoveGenerator";
+
+describe("Move Generator | Bishop", () => {
+  test("White bishop at [1, 1]", () => {
+    const board = new Board("8/8/8/8/8/8/1B6/8").getBoard();
+    const moves = MoveGenerator.getMoves(board, [1, 1]);
+
+    expect(moves.map((m) => m.to)).toEqual([
+      [0, 0],
+      [0, 2],
+      [2, 0],
+      [2, 2],
+      [3, 3],
+      [4, 4],
+      [5, 5],
+      [6, 6],
+      [7, 7],
+    ]);
+  });
+
+  test("Bishop with capture", () => {
+    const board = new Board("7b/8/8/8/8/8/1B6/8").getBoard();
+    const moves = MoveGenerator.getMoves(board, [1, 1]);
+
+    expect(moves.map((m) => m.to)).toEqual([
+      [0, 0],
+      [0, 2],
+      [2, 0],
+      [2, 2],
+      [3, 3],
+      [4, 4],
+      [5, 5],
+      [6, 6],
+      [7, 7],
+    ]);
+  });
+
+  test("Bishop with blocking piece", () => {
+    const board = new Board("8/8/8/8/8/2b5/1B6/8").getBoard();
+    const moves = MoveGenerator.getMoves(board, [1, 1]);
+
+    expect(moves.map((m) => m.to)).toEqual([
+      [0, 0],
+      [0, 2],
+      [2, 0],
+      [2, 2],
+    ]);
+  });
+});
