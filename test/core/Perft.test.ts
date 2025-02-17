d806995b870e6828c0dc1faf4d24813d7342dacb
TetieWasTaken <tjvssr@gmail.com>
2025-02-17 22:36:01 +0100

    feat(test): add debugging perft

--- test/core/Perft.test.ts
+++ test/core/Perft.test.ts
@@ -29,6 +29,62 @@ describe("Perft", () => {
     expect(MoveGenerator._perft(board, 3)).toBe(9467);
   });
 
+  test("d3 c4c5", () => {
+    const board = new Board(
+      "r3k2r/Pppp1ppp/1b3nbN/nPP5/BB2P3/q4N2/Pp1P2PP/R2Q1RK1 b kq - 0 1",
+    );
+
+    expect(MoveGenerator._perft(board, 2)).toBe(1409);
+  });
+
+  test("d3 c4c5", () => {
+    const board = new Board(
+      "r3k2r/Pppp1ppp/1b3nbN/nPP5/BB2P3/q4N2/Pp1P2PP/R2Q1RK1 b kq - 0 1",
+    );
+
+    expect(MoveGenerator._perft(board, 2)).toBe(1409);
+  });
+
+  test("d3 d2d4", () => {
+    const board = new Board(
+      "r3k2r/Pppp1ppp/1b3nbN/nP6/BBPPP3/q4N2/Pp4PP/R2Q1RK1 b kq - 0 1",
+    );
+
+    expect(MoveGenerator._perft(board, 2)).toBe(1643);
+  });
+
+  test("d3 f3d4", () => {
+    const board = new Board(
+      "r3k2r/Pppp1ppp/1b3nbN/nP6/BBPNP3/q7/Pp1P2PP/R2Q1RK1 b kq - 0 1",
+    );
+
+    expect(MoveGenerator._perft(board, 2)).toBe(1687);
+  });
+
+  test("d3 b4c5", () => {
+    const board = new Board(
+      "r3k2r/Pppp1ppp/1b3nbN/nPB5/B1P1P3/q4N2/Pp1P2PP/R2Q1RK1 b kq - 0 1",
+    );
+
+    expect(MoveGenerator._perft(board, 2)).toBe(1352);
+  });
+
+  test("d3 f1f2", () => {
+    const board = new Board(
+      "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P1RPP/R2Q2K1 b kq - 0 1",
+    );
+
+    expect(MoveGenerator._perft(board, 2)).toBe(1623);
+  });
+
+  test("d3 g1h1", () => {
+    const board = new Board(
+      "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1R1K b kq - 0 1",
+    );
+
+    expect(MoveGenerator._perft(board, 2)).toBe(1753);
+  });
+
   /* test("Depth 4", () => {
     const board = new Board(
       position,
