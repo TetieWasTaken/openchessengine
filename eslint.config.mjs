5f7cae46bb748280a29b355d443b61209ba53edd
TetieWasTaken <tjvssr@gmail.com>
2025-02-18 22:39:31 +0100

    feat: enforce strict lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,7 +28,7 @@ export default [
         "import/resolver": {
           "typescript": true,
           "node": true,
-        }
+        },
       },
       rules: {
         ...importPlugin.configs?.recommended?.rules ?? {},
@@ -44,4 +44,44 @@ export default [
       },
     }
   ),
+  {
+    rules: {
+      eqeqeq: 'error',
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'prefer-arrow-callback': 'error',
+      'prefer-template': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unsafe-call': 'error',
+      '@typescript-eslint/no-unsafe-member-access': 'error',
+      '@typescript-eslint/no-unsafe-return': 'error',
+      '@typescript-eslint/no-unsafe-type-assertion': 'error',
+      '@typescript-eslint/prefer-for-of': 'error',
+      '@typescript-eslint/prefer-nullish-coalescing': 'error',
+      '@typescript-eslint/prefer-optional-chain': 'error',
+      '@typescript-eslint/restrict-plus-operands': [
+        'error',
+        {
+          allowAny: false,
+          allowBoolean: false,
+          allowNullish: false,
+          allowNumberAndString: false,
+          allowRegExp: false,
+        },
+      ],
+      '@typescript-eslint/restrict-template-expressions': 'error',
+      '@typescript-eslint/strict-boolean-expressions': [
+        'error',
+        {
+          allowNumber: false,
+          allowString: false,
+        },
+      ],
+      '@typescript-eslint/use-unknown-in-catch-callback-variable': 'error',
+    },
+  },
 ];
