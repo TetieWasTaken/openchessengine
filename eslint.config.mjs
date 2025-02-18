db681d62f4f430d02554005c65367c09697b1b30
TetieWasTaken <tjvssr@gmail.com>
2025-02-18 22:09:07 +0100

    chore: do not allow default export

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,19 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import * as importPlugin from 'eslint-plugin-import';
 
 export default [
   {
-    ignores: ['node_modules/', 'dist/', 'babel.config.js'],
+    ignores: ['node_modules/', 'dist/', 'babel.config.js', 'eslint.config.mjs'],
+  },
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/no-default-export': 'error'
+    }
   },
   ...tseslint.config(
     eslint.configs.recommended,
