d76f70f3733f728d545f50374e88b97467e6656e
TetieWasTaken <tjvssr@gmail.com>
2025-02-19 15:24:03 +0100

    fix(cli.test): lint

--- test/interfaces/cli.test.ts
+++ test/interfaces/cli.test.ts
@@ -1,28 +1,44 @@
-/* eslint-disable no-console */
+import { exec, type ExecException } from "node:child_process";
 
-import { exec, ExecException } from "node:child_process";
+async function execPromise(
+  command: string,
+): Promise<{ stderr: string; stdout: string }> {
+  return new Promise((resolve, reject) => {
+    exec(
+      command,
+      (error: ExecException | null, stdout: string, stderr: string) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        resolve({ stdout, stderr });
+      },
+    );
+  });
+}
 
 describe("cli", () => {
   it("Should return new fen", (done) => {
     const fen = "6k1/8/8/8/8/3K4/4R3/5R2 w - - 0 1";
     const expectedFen = "6k1/8/8/8/8/3K4/6R1/5R2 b - - 1 1";
 
-    exec(
-      `npm run start -- -f "${fen}" -d 4`,
-      (error: ExecException | null, stdout: string): void => {
-        if (error) {
+    (async () => {
+      try {
+        const { stdout } = await execPromise(
+          `npm run start -- -f "${fen}" -d 4`,
+        );
+        expect(stdout).toContain(expectedFen);
+        await done();
+      } catch (error) {
+        if (error instanceof Error) {
           console.error(`exec error: ${error.message}`);
-          done(error);
-          return;
+        } else {
+          console.error(`exec error: ${error}`);
         }
 
-        try {
-          expect(stdout).toContain(expectedFen);
-          done();
-        } catch (err) {
-          done(err);
-        }
-      },
-    );
-  }, 10000);
+        done(error);
+      }
+    })();
+  }, 10_000);
 });
