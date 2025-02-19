f02e435f86d92016fa86cb53541fb91acdb6e8a2
TetieWasTaken <tjvssr@gmail.com>
2025-02-19 17:19:35 +0100

    fix: remove conflicting import/order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,13 +15,6 @@ export default [
     },
     rules: {
       'import/no-default-export': 'error',
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          alphabetize: { order: 'asc', caseInsensitive: true },
-        },
-      ],
     },
   },
   ...tseslint.config(
