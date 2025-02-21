c852ce3b365979a0121848f9059b8c6c89311a1b
TetieWasTaken <tjvssr@gmail.com>
2025-02-21 22:16:04 +0100

    fix(test/search): use toMatchObject

--- test/bot/Search.test.ts
+++ test/bot/Search.test.ts
@@ -9,12 +9,12 @@ describe('Search', () => {
 	test('Checkmate in 1', () => {
 		const board = new Board('kr6/pp6/8/3N4/8/8/8/4K3 w - - 0 1');
 		const move = search(board, 3);
-		expect(move).toContain({ from: [3, 3], to: [2, 1] });
+		expect(move).toMatchObject({ from: [3, 3], to: [2, 1] });
 	});
 
 	test('Four pieces', () => {
 		const board = new Board('k7/3n4/8/1q1R1p2/3b4/8/8/7K w - - 0 1');
 		const move = search(board, 3);
-		expect(move).toContain({ from: [3, 3], to: [1, 3] });
+		expect(move).toMatchObject({ from: [3, 3], to: [1, 3] });
 	});
 });
