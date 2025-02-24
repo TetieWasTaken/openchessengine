e4d5a7c6179442d1d311762d8329637e2e177ae8
TetieWasTaken <tjvssr@gmail.com>
2025-02-24 19:45:20 +0100

    fix(test/rook): coordinate system

--- test/core/MoveGenerator/rook.test.ts
+++ test/core/MoveGenerator/rook.test.ts
@@ -4,48 +4,33 @@ import { Board } from '../../../src/core/board';
 import { getMoves } from '../../../src/core/moveGenerator';
 
 describe('Move Generator | Rook', () => {
-	test('White rook at [1, 1]', () => {
-		const board = new Board('8/8/8/8/8/8/1R6/8 w - - 0 1');
-		const moves = getMoves(board, [1, 1]);
+	test('Regular rook', () => {
+		const board = new Board('8/8/8/8/4R3/8/8/8 w - - 0 1');
+		const moves = getMoves(board, [4, 4]);
 
-		// Ignore the order of the moves, as it doesn't matter
 		const expectedMoves = [
-			[0, 1],
-			[2, 1],
-			[3, 1],
-			[4, 1],
-			[5, 1],
-			[6, 1],
-			[7, 1],
-			[1, 0],
-			[1, 2],
-			[1, 3],
-			[1, 4],
-			[1, 5],
-			[1, 6],
-			[1, 7],
+			[4, 0],
+			[0, 4],
+			[7, 4],
+			[4, 7],
 		];
 
-		for (const move of expectedMoves) {
-			expect(moves.map((move) => move.to)).toContainEqual(move);
-		}
-
-		expect(moves.length).toBe(expectedMoves.length);
+		expect(moves.map((move) => move.to)).toEqual(expect.arrayContaining(expectedMoves));
 	});
 
 	test('Rook with capture', () => {
-		const board = new Board('8/1r6/8/8/8/8/1R6/8 w - - 0 1');
-		const moves = getMoves(board, [1, 1]);
+		const board = new Board('8/4p3/8/8/4R3/8/8/8 w - - 0 1');
+		const moves = getMoves(board, [4, 4]);
 
-		expect(moves.map((move) => move.to)).toContainEqual([6, 1]);
-		expect(moves.map((move) => move.to)).not.toContainEqual([7, 1]);
+		expect(moves.map((move) => move.to)).toContainEqual([4, 1]);
+		expect(moves.map((move) => move.to)).not.toContainEqual([4, 0]);
 	});
 
 	test('Rook with blocking piece', () => {
-		const board = new Board('8/1R6/8/8/8/8/1R6/8 w - - 0 1');
-		const moves = getMoves(board, [1, 1]);
+		const board = new Board('8/4P3/8/8/4R3/8/8/8 w - - 0 1');
+		const moves = getMoves(board, [4, 4]);
 
-		expect(moves.map((move) => move.to)).not.toContainEqual([6, 1]);
-		expect(moves.map((move) => move.to)).not.toContainEqual([7, 1]);
+		expect(moves.map((move) => move.to)).not.toContainEqual([4, 1]);
+		expect(moves.map((move) => move.to)).not.toContainEqual([4, 0]);
 	});
 });
