83793b28aac8a6197362cb608d478a42e8d31aa2
TetieWasTaken <tjvssr@gmail.com>
2025-02-24 19:11:56 +0100

    fix(core/knight): coordinate system

--- test/core/MoveGenerator/knight.test.ts
+++ test/core/MoveGenerator/knight.test.ts
@@ -4,38 +4,35 @@ import { Board } from '../../../src/core/board';
 import { getMoves } from '../../../src/core/moveGenerator';
 
 describe('Move Generator | Knight', () => {
-	test('White knight at [1, 1]', () => {
-		const board = new Board('8/8/8/8/8/8/1N6/8 w - - 0 1');
-		const moves = getMoves(board, [1, 1]);
+	test('White knight', () => {
+		const board = new Board('8/8/2N5/8/8/8/8/8 w - - 0 1');
+		const moves = getMoves(board, [2, 2]);
 
-		expect(moves.map((move) => move.to)).toEqual([
+		const expectedMoves = [
+			[0, 1],
 			[0, 3],
-			[2, 3],
+			[1, 0],
+			[1, 4],
 			[3, 0],
-			[3, 2],
-		]);
+			[3, 4],
+			[4, 1],
+			[4, 3],
+		];
+
+		expect(moves.map((move) => move.to)).toEqual(expect.arrayContaining(expectedMoves));
 	});
 
 	test('Knight with capture', () => {
-		const board = new Board('8/8/8/8/n7/8/1N6/8 w - - 0 1');
-		const moves = getMoves(board, [1, 1]);
+		const board = new Board('1p6/8/2N5/8/8/8/8/8 w - - 0 1');
+		const moves = getMoves(board, [2, 2]);
 
-		expect(moves.map((move) => move.to)).toEqual([
-			[0, 3],
-			[2, 3],
-			[3, 0],
-			[3, 2],
-		]);
+		expect(moves.map((move) => move.to)).toContainEqual([0, 1]);
 	});
 
 	test('Knight with blocking piece', () => {
-		const board = new Board('8/8/8/8/P7/8/1N6/8 w - - 0 1');
-		const moves = getMoves(board, [1, 1]);
+		const board = new Board('1P6/8/2N5/8/8/8/8/8 w - - 0 1');
+		const moves = getMoves(board, [2, 2]);
 
-		expect(moves.map((move) => move.to)).toEqual([
-			[0, 3],
-			[2, 3],
-			[3, 2],
-		]);
+		expect(moves.map((move) => move.to)).not.toContainEqual([0, 1]);
 	});
 });
