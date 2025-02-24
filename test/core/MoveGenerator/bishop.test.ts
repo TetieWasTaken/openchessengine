c9d3eb5287bccdf0c5c79aea256dded36851a2ee
TetieWasTaken <tjvssr@gmail.com>
2025-02-24 17:10:29 +0100

    fix(test/bishop): coordinate system

--- test/core/MoveGenerator/bishop.test.ts
+++ test/core/MoveGenerator/bishop.test.ts
@@ -4,49 +4,53 @@ import { Board } from '../../../src/core/board';
 import { getMoves } from '../../../src/core/moveGenerator';
 
 describe('Move Generator | Bishop', () => {
-	test('White bishop at [1, 1]', () => {
-		const board = new Board('8/8/8/8/8/8/1B6/8 w - - 0 1');
-		const moves = getMoves(board, [1, 1]);
+	test('White bishop', () => {
+		const board = new Board('8/8/8/8/4B3/8/8/8 w - - 0 1');
+		const moves = getMoves(board, [4, 4]);
 
-		expect(moves.map((move) => move.to)).toEqual([
+		const expectedMoves = [
 			[0, 0],
-			[0, 2],
-			[2, 0],
+			[1, 1],
 			[2, 2],
 			[3, 3],
-			[4, 4],
 			[5, 5],
 			[6, 6],
 			[7, 7],
-		]);
+			[1, 7],
+			[2, 6],
+			[3, 5],
+			[5, 3],
+			[6, 2],
+			[7, 1],
+		];
+
+		expect(moves.map((move) => move.to)).toEqual(expect.arrayContaining(expectedMoves));
 	});
 
 	test('Bishop with capture', () => {
-		const board = new Board('7b/8/8/8/8/8/1B6/8 w - - 0 1');
-		const moves = getMoves(board, [1, 1]);
+		const board = new Board('8/8/8/8/8/2b5/1B6/8 w - - 0 1');
+		const moves = getMoves(board, [1, 6]);
 
-		expect(moves.map((move) => move.to)).toEqual([
-			[0, 0],
-			[0, 2],
-			[2, 0],
-			[2, 2],
-			[3, 3],
-			[4, 4],
-			[5, 5],
-			[6, 6],
-			[7, 7],
-		]);
+		const expectedMoves = [
+			[0, 7],
+			[0, 5],
+			[2, 7],
+			[2, 5],
+		];
+
+		expect(moves.map((move) => move.to)).toEqual(expect.arrayContaining(expectedMoves));
 	});
 
 	test('Bishop with blocking piece', () => {
-		const board = new Board('8/8/8/8/8/2b5/1B6/8 w - - 0 1');
-		const moves = getMoves(board, [1, 1]);
+		const board = new Board('8/8/8/8/8/2B5/1B6/8 w - - 0 1');
+		const moves = getMoves(board, [1, 6]);
 
-		expect(moves.map((move) => move.to)).toEqual([
-			[0, 0],
-			[0, 2],
-			[2, 0],
-			[2, 2],
-		]);
+		const expectedMoves = [
+			[0, 5],
+			[0, 7],
+			[2, 7],
+		];
+
+		expect(moves.map((move) => move.to)).toEqual(expect.arrayContaining(expectedMoves));
 	});
 });
